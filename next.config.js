3d3bb231d8bad01b722b29f814b81c6bda3ba791
HanSeo0507 <i.hansu.lee@gmail.com>
2022-02-17 22:11:47 +0900

    feat: add @svgr/webpack plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
 
-module.exports = nextConfig
+	webpack(conf) {
+		conf.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return conf;
+	},
+};
+
+module.exports = nextConfig;
