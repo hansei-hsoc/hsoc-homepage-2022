af90f20155697bb0e7cf246abafbd431ad94267b
HanSeo0507 <i.hansu.lee@gmail.com>
2022-02-17 20:03:58 +0900

    style(edit): modify global styles

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -8,11 +8,23 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    :root {
+        --color-primary: #6C51FF;
+    }
+
     html, body, #__next {
         width: 100%;
         height: 100%;
         font-size: 16px;
         font-weight: 400;
         font-family: 'Spoqa Han Sans Neo', sans-serif;
+        background: #0E0F13;
+        color: white;
+    }
+
+    .container {
+        width: 100%;
+        max-width: 1280px;
+        margin: 0 auto;
     }
 `;
