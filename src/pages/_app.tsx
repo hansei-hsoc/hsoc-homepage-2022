834910010bc5b5950f003daeccecd939153b7240
HanSeo0507 <i.hansu.lee@gmail.com>
2022-03-07 02:53:45 +0900

    feat: add seo tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Navbar } from "src/components";
@@ -8,8 +9,21 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>한세사이버보안고등학교 보안관제 동아리</title>
 			</Head>
+			<DefaultSeo
+				title="HSOC | 한세사이버보안고등학교 보안관제 동아리"
+				description="한세사이버보안고등학교 보안관제 동아리"
+				canonical="https://hsoc.kr"
+				openGraph={{
+					type: "website",
+					locale: "ko_KR",
+					url: "https://hsoc.kr",
+					title: "HSOC | 한세사이버보안고등학교 보안관제 동아리",
+					description: "한세사이버보안고등학교 보안관제 동아리",
+					images: [{ url: "https://hsoc.kr/og_thumbnail.png", width: 1200, height: 630 }],
+					site_name: "HSOC | 한세사이버보안고등학교 보안관제 동아리",
+				}}
+			/>
 			<GlobalStyle />
 			<Navbar />
 			<Component {...pageProps} />
